b875e8ef42e5cde7c997a3510335ea1ae9cb3843
cdio3 <tnquanganh@gmail.com>
2025-01-11 23:51:17 +0700

    up

--- app/src/main/java/com/example/foodapp/ChooseLocationActivity.kt
+++ app/src/main/java/com/example/foodapp/ChooseLocationActivity.kt
@@ -1,15 +1,20 @@
 package com.example.foodapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.foodapp.databinding.ActivityChooseLocationBinding
 
 class ChooseLocationActivity : AppCompatActivity() {
+    private val binding: ActivityChooseLocationBinding by lazy {
+        ActivityChooseLocationBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_location)
-
+        val locationList:Array<String> = arrayOf("Hà Nội", "Đà Nẵng", "Hồ Chí Minh")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,locationList)
+        val autoCloseableTextView = binding.listOfLocation
+        autoCloseableTextView.setAdapter(adapter)
     }
 }
---
+++ app/src/main/java/com/example/foodapp/Fragment/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.example.foodapp.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.foodapp.R
+
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    companion object {
+
+    }
+}
